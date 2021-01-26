a1a837ff343ff33b87b9d16e6939fdc0b7a0681a
demid-ns <siwi1992@gmail.com>
2021-01-26 17:11:42 +0200

    DB context

---
+++ TPDB.Auth.API/Data/TPDBAuthContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TPDB.Auth.API.Models;
+
+namespace TPDB.Auth.API.Data
+{
+    public class TPDBAuthContext: DbContext
+    {
+        public DbSet<Account> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        public TPDBAuthContext(DbContextOptions<TPDBAuthContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
