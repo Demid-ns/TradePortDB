01ac6d8e9ca1e81d944e37de1fd46172d1b8f1ed
demid-ns <siwi1992@gmail.com>
2021-01-26 17:13:42 +0200

    DBContext

---
+++ TPDB.Resource.API/Data/TPDBResourceContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TPDB.Resource.API.Models;
+
+namespace TPDB.Resource.API.Data
+{
+    public class TPDBResourceContext : DbContext
+    {
+        public DbSet<Port> Ports { get; set; }
+        public DbSet<Commodity> Commodities { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public TPDBResourceContext(DbContextOptions<TPDBResourceContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
