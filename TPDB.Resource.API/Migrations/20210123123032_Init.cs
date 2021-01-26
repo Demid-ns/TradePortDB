0279ca30b12191a056faf2564a63ef08f00e2a40
demid-ns <siwi1992@gmail.com>
2021-01-26 17:13:52 +0200

    Migrations

---
+++ TPDB.Resource.API/Migrations/20210123123032_Init.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TPDB.Resource.API.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    System = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Body = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ports", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Commodities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<int>(type: "int", nullable: true),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PortId = table.Column<int>(type: "int", nullable: true),
+                    PortId1 = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commodities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Commodities_Ports_PortId",
+                        column: x => x.PortId,
+                        principalTable: "Ports",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Commodities_Ports_PortId1",
+                        column: x => x.PortId1,
+                        principalTable: "Ports",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Commodities_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Commodities_PortId",
+                table: "Commodities",
+                column: "PortId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Commodities_PortId1",
+                table: "Commodities",
+                column: "PortId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Commodities_ProductId",
+                table: "Commodities",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Commodities");
+
+            migrationBuilder.DropTable(
+                name: "Ports");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ TPDB.Resource.API/Migrations/20210123130147_ForImportMigration.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TPDB.Resource.API.Migrations
+{
+    public partial class ForImportMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Commodities_Ports_PortId1",
+                table: "Commodities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Commodities_PortId1",
+                table: "Commodities");
+
+            migrationBuilder.DropColumn(
+                name: "PortId1",
+                table: "Commodities");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ForImport",
+                table: "Commodities",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ForImport",
+                table: "Commodities");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PortId1",
+                table: "Commodities",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Commodities_PortId1",
+                table: "Commodities",
+                column: "PortId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Commodities_Ports_PortId1",
+                table: "Commodities",
+                column: "PortId1",
+                principalTable: "Ports",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
