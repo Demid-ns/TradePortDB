f6f3ddb14a6647b235a22e8e7db11f7a1d8d802a
demid-ns <siwi1992@gmail.com>
2021-01-28 22:26:19 +0200

    routes

--- TPDB-Client/src/app/app-routing.module.ts
+++ TPDB-Client/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
+import {PortsComponent} from './ports/ports.component';
+import {ProductsComponent} from './products/products.component';
+import {CommoditiesComponent} from './commodities/commodities.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: '/ports', pathMatch: 'full'},
+      {path: 'ports', component: PortsComponent},
+      {path: 'products', component: ProductsComponent},
+      {path: 'commodities', component: CommoditiesComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
