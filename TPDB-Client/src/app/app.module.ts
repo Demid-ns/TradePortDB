5a7d1929e1cc0b766dcf221750b43f49dac06c86
demid-ns <siwi1992@gmail.com>
2021-01-28 22:27:05 +0200

    imports & declarations

--- TPDB-Client/src/app/app.module.ts
+++ TPDB-Client/src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
+import {PortsComponent} from './ports/ports.component';
+import {ProductsComponent} from './products/products.component';
+import {CommoditiesComponent} from './commodities/commodities.component';
+import {LoginFormComponent} from './login-form/login-form.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortsComponent,
+    ProductsComponent,
+    CommoditiesComponent,
+    LoginFormComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
