72afc9cdff0d66ebce15c1c0f24a467205bafda4
demid-ns <siwi1992@gmail.com>
2021-01-29 17:29:28 +0200

    new declarations & imports

--- TPDB-Client/src/app/app.module.ts
+++ TPDB-Client/src/app/app.module.ts
@@ -8,10 +8,17 @@ import {HttpClientModule} from '@angular/common/http';
 import {PortsComponent} from './ports/ports.component';
 import {ProductsComponent} from './products/products.component';
 import {CommoditiesComponent} from './commodities/commodities.component';
-import {LoginFormComponent} from './login-form/login-form.component';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
+import {LoginComponent, LoginDialogComponent} from './login/login.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -19,16 +26,25 @@ import {MatCardModule} from '@angular/material/card';
     PortsComponent,
     ProductsComponent,
     CommoditiesComponent,
-    LoginFormComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    LoginComponent,
+    LoginDialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatToolbarModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
