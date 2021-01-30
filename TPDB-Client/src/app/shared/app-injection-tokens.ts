147f37c010ca78f35c3eb2a1bd3c9aa2c231dc76
demid-ns <siwi1992@gmail.com>
2021-01-30 18:17:14 +0200

    API Url's

---
+++ TPDB-Client/src/app/shared/app-injection-tokens.ts
@@ -0,0 +1,4 @@
+import {InjectionToken} from '@angular/core';
+
+export const AUTH_API_URL = new InjectionToken('auth api url');
+export const RES_API_URL = new InjectionToken('resource server api url');
