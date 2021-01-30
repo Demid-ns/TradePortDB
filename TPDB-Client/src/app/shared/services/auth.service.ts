4927a53c307a6087e320983d1d131477b2909f5a
demid-ns <siwi1992@gmail.com>
2021-01-30 18:20:52 +0200

    auth service rewrited

--- TPDB-Client/src/app/shared/services/auth.service.ts
+++ TPDB-Client/src/app/shared/services/auth.service.ts
@@ -1,22 +1,30 @@
-import {Injectable} from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {User} from '../interfaces/user';
+import {APIResponse, User} from '../interfaces/interfaces';
 import {Observable} from 'rxjs';
+import {tap} from 'rxjs/operators';
+import {AUTH_API_URL} from '../app-injection-tokens';
+import {JwtHelperService} from '@auth0/angular-jwt';
+import {Router} from '@angular/router';
+
+export const LOCAL_TOKEN_KEY = 'tpdb_access_token';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
-  get token(): string {
-    return '';
+  constructor(private http: HttpClient,
+              @Inject(AUTH_API_URL) private apiUrl: string,
+              private jwtHelper: JwtHelperService,
+              private router: Router) {
   }
 
-  constructor(private http: HttpClient) {
-  }
-
-  login(user: User): Observable<any> {
-    return this.http.post('', user);
+  login(user: User): Observable<APIResponse> {
+    user.returnJWTToken = true;
+    return this.http.post(`${this.apiUrl}api/auth/login`, user)
+      .pipe(
+        tap(this.setToken)
+      );
   }
 
   logout(): void {
@@ -24,11 +32,13 @@ export class AuthService {
   }
 
   isAuthenticated(): boolean {
-    return !!this.token;
+    const token = localStorage.getItem(LOCAL_TOKEN_KEY);
+    return token && !this.jwtHelper.isTokenExpired(token);
   }
 
-  private setToken() {
-
+  private setToken(response: APIResponse): void {
+    console.log(response.access_token)
+    localStorage.setItem(LOCAL_TOKEN_KEY, response.access_token);
   }
 
 }
