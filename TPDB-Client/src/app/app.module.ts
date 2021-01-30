c3317644ea793c78976cd315b62c41d6c7b9f65f
demid-ns <siwi1992@gmail.com>
2021-01-30 18:13:25 +0200

    new providers & declarations

--- TPDB-Client/src/app/app.module.ts
+++ TPDB-Client/src/app/app.module.ts
@@ -8,7 +8,8 @@ import {HttpClientModule} from '@angular/common/http';
 import {PortsComponent} from './ports/ports.component';
 import {ProductsComponent} from './products/products.component';
 import {CommoditiesComponent} from './commodities/commodities.component';
-import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
+
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -19,6 +20,15 @@ import {LoginComponent, LoginDialogComponent} from './login/login.component';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDialogModule} from '@angular/material/dialog';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import {AUTH_API_URL, RES_API_URL} from './shared/app-injection-tokens';
+import {environment} from '../environments/environment';
+import {JwtModule} from '@auth0/angular-jwt';
+import {LOCAL_TOKEN_KEY} from './shared/services/auth.service';
+
+export function tokenGetter(): string {
+  return localStorage.getItem(LOCAL_TOKEN_KEY);
+}
 
 @NgModule({
   declarations: [
@@ -37,6 +47,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
+
     MatToolbarModule,
     MatCardModule,
     MatFormFieldModule,
@@ -44,9 +55,20 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
     MatGridListModule,
     MatButtonModule,
     MatIconModule,
-    MatDialogModule
+    MatDialogModule,
+
+    JwtModule.forRoot({
+      config: {tokenGetter,
+      allowedDomains: environment.tokenAllowedDomains}
+    })
   ],
-  providers: [],
+  providers: [{
+    provide: AUTH_API_URL,
+    useValue: environment.authApi
+  }, {
+    provide: RES_API_URL,
+    useValue: environment.resApi
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
