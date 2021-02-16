96e18f02690706ce89aa1d268ffffd3a673e0bd1
demid-ns <siwi1992@gmail.com>
2021-02-16 13:34:08 +0200

    service http client

--- TPDB-Client/src/app/shared/services/commodity.service.ts
+++ TPDB-Client/src/app/shared/services/commodity.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {Product} from '../models/product';
 import {environment} from '../../../environments/environment';
+import {Commodity} from '../models/commodity';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommodityService {
   constructor(private http: HttpClient) {
+  }
+
+  public getCommodities(): Observable<Commodity[]> {
+    return this.http.get<Commodity[]>(`${environment.resApi}api/commodities`);
+  }
+
+  public getCommodity(id: number): Observable<Commodity>{
+    return this.http.get<Commodity>(`${environment.resApi}api/commodities/${id}`);
+  }
+
+  public removeCommodity(id: number): Observable<void>{
+    return this.http.delete<void>(`${environment.resApi}api/commodities/${id}`);
+  }
+
+  public addCommodity(commodity: Commodity): Observable<Commodity> {
+    return this.http.post<Commodity>(`${environment.resApi}api/commodities`, commodity);
   }
 }
